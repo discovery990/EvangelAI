b599c4d84231555c139fe4a7e120f060863e4d2b
discovery990 <danieldadzie5@gmail.com>
2025-07-29 16:11:22 +0100

    Add project dashboard and serve static index

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the main dashboard page
 app.get('/', (req, res) => {
-    res.send('EvangelAI is running...');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
